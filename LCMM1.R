5dd34e5b8c9a5bfa5089c8244ac1e370e44099cb
Rachel Lee <rachellee@Rachels-MBP.fios-router.home>
2020-06-30 02:36:44 -0400

    varying # of ng's with link functions (linear, beta, spline3, spline5)

--- LCMM1.R
+++ LCMM1.R
@@ -13,16 +13,61 @@ cmpst_all2 <- read_csv(file = "/Users/rachellee/Google Drive/Practicum/Data/cmps
 cmpst_all2$visit <- revalue(cmpst_all2$visit, c("Baseline"=0, "Week3" = 3, "Week6" =6, "Week9"=9, "Week14"=14)) %>%
   as.numeric(cmpst_all2$visit)
 
+cmpst_all2 <- data.frame(cmpst_all2)
+
 head(cmpst_all2)
+head(cmpst_all2$patient_id)
+class(cmpst_all2$patient_id)
 
 summary(cmpst_all2)
 
+#####################################################
+# ng = 2
+#####################################################
+#linear link function
+hamd_linear <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 2, data = cmpst_all2, link = "linear", idiag = FALSE, na.action = 1)
+summary(hamd_linear)
+plot(hamd_linear, which="linkfunction",bty="l")
 
-#exposure: madrs_total, pos_affect, neg_affect
-#outcome: ham24tot
+#beta link function
+hamd_beta <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 2, data = cmpst_all2, link = "beta", idiag = FALSE, na.action = 1)
+summary(hamd_beta)
+plot(hamd_beta, which="linkfunction",bty="l")
 
-lcmm(ham24tot ~ visit + madrs_total, mixture= 'visit', subject = 'patient_id', ng = 2, data = cmpst_all2)
+# I-splines with 3 equidistant nodes
+hamd_spline3 <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 2, data = cmpst_all2, link="3-equi-splines", idiag = FALSE, na.action = 1)
+summary(hamd_spline3)
+plot(hamd_spline3, which = "linkfunction", bty = "l")
+
+
+# I-splines with 5 nodes at quantiles
+hamd_spline5 <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 2, data = cmpst_all2, link="5-quant-splines", idiag = FALSE, na.action = 1)
+summary(hamd_spline5)
+plot(hamd_spline5, which = "linkfunction", bty = "l")
 
 
 
+#####################################################
+# ng = 3
+#####################################################
+#linear link function
+hamd_linear <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 3, data = cmpst_all2, link = "linear", idiag = FALSE, na.action = 1)
+summary(hamd_linear)
+plot(hamd_linear, which="linkfunction",bty="l")
 
+#beta link function
+hamd_beta <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 3, data = cmpst_all2, link = "beta", idiag = FALSE, na.action = 1)
+summary(hamd_beta)
+plot(hamd_beta, which="linkfunction",bty="l")
+
+# I-splines with 3 equidistant nodes
+hamd_spline3 <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 3, data = cmpst_all2, link="3-equi-splines", idiag = FALSE, na.action = 1)
+summary(hamd_spline3)
+plot(hamd_spline3, which = "linkfunction", bty = "l")
+
+
+# I-splines with 5 nodes at quantiles
+
+hamd_spline5 <- lcmm(ham24tot ~ visit, random = ~visit, subject = 'patient_id', mixture = ~visit, ng = 3, data = cmpst_all2, link="5-quant-splines", idiag = FALSE, na.action = 1)
+summary(hamd_spline5)
+plot(hamd_spline5, which = "linkfunction", bty = "l")
